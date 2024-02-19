590c1104cc4061a5c23e37e2fa39c1dca34c46a5
shubham <singhshubham3006.ss@gmail.com>
2024-02-19 11:47:46 +0530

    Initial commit

---
+++ lib/services/location.dart
@@ -0,0 +1,18 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  late double? latitude;
+  late double? longitude;
+  Location({this.latitude, this.longitude});
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      print(position);
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
